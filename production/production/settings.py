998b89f41f0c0cf88bc7e868fca63aac4c8c36c5
elogy <tim.beckmann@student.uni-tuebingen.de>
2018-05-28 20:07:00 +0200

    initial working version with package

--- production/production/settings.py
+++ production/production/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'production/static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'production/static')
+STATIC_ROOT = '/home/pi/fse-production/production/production/static'
 
 MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'production/uploads')
 
